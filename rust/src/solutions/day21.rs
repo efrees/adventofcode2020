922ec1aea5927c782159e3bb8df70197e0e1c611
Eric Frees <efrees@atlasrfid.com>
2020-12-21 22:19:30 -0600

    Solve day 21, part 2

--- rust/src/solutions/day21.rs
+++ rust/src/solutions/day21.rs
@@ -31,9 +31,10 @@ pub fn solve() {
     }
 
     let mut ingredients_with_alergen = HashSet::new();
+    let mut ingredient_alergen_pairs = HashSet::new();
 
     while possible_ingredients.len() > 0 {
-        let alergen = possible_ingredients
+        let alergen: &str = possible_ingredients
             .iter()
             .filter(|(_alergen, set)| set.len() == 1)
             .map(|(&alergen, _set)| alergen)
@@ -43,18 +44,26 @@ pub fn solve() {
         let set_of_one = possible_ingredients.remove(alergen).unwrap();
         let ingredient = set_of_one.iter().cloned().next().unwrap();
         ingredients_with_alergen.insert(ingredient);
+        ingredient_alergen_pairs.insert((ingredient, alergen));
 
         for ingredient_list in possible_ingredients.values_mut() {
             ingredient_list.remove(ingredient);
         }
     }
 
-    let answer = full_ingredient_listing
+    let safe_count = full_ingredient_listing
         .iter()
         .filter(|&ingredient| !ingredients_with_alergen.contains(ingredient))
         .count();
 
-    println!("Output (part 1): {}", answer);
+    println!("Output (part 1): {}", safe_count);
 
-    println!("Output (part 2): {}", -1);
+    let mut unsafe_ingredients: Vec<_> = ingredient_alergen_pairs.iter().cloned().collect();
+    unsafe_ingredients.sort_by_key(|(_i, a)| *a);
+    let canonical_list: Vec<_> = unsafe_ingredients
+        .iter()
+        .cloned()
+        .map(|(i, _a)| i)
+        .collect();
+    println!("Output (part 2): {}", canonical_list.join(","));
 }
