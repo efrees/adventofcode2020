6ac167d476b87d41b8d6f3857e953f358187aef8
Eric Frees <efrees@atlasrfid.com>
2021-01-02 11:23:08 -0600

    Solve day 1 in c#

--- csharp/AdventOfCode2020/Solvers/Day01Solver.cs
+++ csharp/AdventOfCode2020/Solvers/Day01Solver.cs
@@ -10,7 +10,57 @@ namespace AdventOfCode2020.Solvers
         public void Solve()
         {
             var lines = Input.GetLinesFromFile(InputFile);
+            var sortedNumbers = lines.Select(long.Parse).OrderBy(x => x).ToArray();
+
+            Console.WriteLine($"Output (part 1): {GetPart1Answer(sortedNumbers)}");
+            Console.WriteLine($"Output (part 2): {GetPart2Answer(sortedNumbers)}");
         }
 
+        private static long GetPart1Answer(long[] sortedNumbers)
+        {
+            var (i, j) = FindIndexOfPairWithSum(sortedNumbers, 2020);
+            return sortedNumbers[i] * sortedNumbers[j];
+        }
+
+        private static (int i, int j) FindIndexOfPairWithSum(long[] sortedNumbers, long target)
+        {
+            var i = 0;
+            var j = sortedNumbers.Length - 1;
+            while (i < j)
+            {
+                var sum = sortedNumbers[i] + sortedNumbers[j];
+                if (sum == target)
+                {
+                    break;
+                }
+
+                if (sum < target)
+                {
+                    i++;
+                }
+
+                j--;
+            }
+
+            return i < j
+                ? (i, j)
+                : (-1, -1);
+        }
+
+        private static long GetPart2Answer(long[] sortedNumbers)
+        {
+            for (var i = 0; i < sortedNumbers.Length - 2; i++)
+            {
+                var target = 2020 - sortedNumbers[i];
+
+                var (j, k) = FindIndexOfPairWithSum(sortedNumbers[(i+1)..], target);
+                if (j > -1)
+                {
+                    return sortedNumbers[i] * sortedNumbers[i + j + 1] * sortedNumbers[i + k + 1];
+                }
+            }
+
+            throw new InvalidOperationException("Answer not found");
+        }
     }
 }
