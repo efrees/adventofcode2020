f9aa4908acc3c33027a0471769178a2ebd337239
Eric Frees <efrees@atlasrfid.com>
2021-01-02 13:06:35 -0600

    Fix input path for command line

--- csharp/AdventOfCode2020/Input.cs
+++ csharp/AdventOfCode2020/Input.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace AdventOfCode2020
 {
     public static class Input
     {
+        private static readonly string BinDirectory = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+
         internal static string GetInputFromFile(string filename)
         {
-            return File.ReadAllText("InputFiles/" + filename);
+            var absolutePath = Path.Combine(BinDirectory, $"InputFiles/{filename}");
+            return File.ReadAllText(absolutePath);
         }
 
         internal static IEnumerable<string> GetLinesFromFile(string filename)
