b763ee22cca54ea9d325a99a180dcf65f18c78ce
Eric Frees <efrees@atlasrfid.com>
2021-01-03 15:33:11 -0600

    Solve day 11, part 2 in C#

--- csharp/AdventOfCode2020/Solvers/Day11Solver.cs
+++ csharp/AdventOfCode2020/Solvers/Day11Solver.cs
@@ -46,14 +46,76 @@ namespace AdventOfCode2020.Solvers
 
                     var occupiedNeighbors = GetDirections()
                         .Select(dir => AddVectors(dir, (col, row)))
-                        .Where(position => position.x >= 0 && position.x < currentLayout[row].Length
-                                                           && position.y >= 0 && position.y < currentLayout.Length)
+                        .Where(position => IsInBounds(position, currentLayout[row].Length, currentLayout.Length))
                         .Count(position => currentLayout[position.y][position.x] == '#');
 
                     var newState = currentSeat switch
                     {
                         'L' when occupiedNeighbors == 0 => '#',
                         '#' when occupiedNeighbors >= 4 => 'L',
+                        _ => currentSeat
+                    };
+
+                    if (newState != currentSeat)
+                    {
+                        seatChanged = true;
+                    }
+
+                    nextLayout[row][col] = newState;
+                }
+            }
+
+            return seatChanged;
+        }
+
+        private long GetPart2Answer(List<string> rows)
+        {
+            var currentLayout = rows.Select(row => row.ToCharArray()).ToArray();
+            var nextLayout = rows.Select(row => row.ToCharArray()).ToArray();
+
+            while (SimulateRoundPart2(currentLayout, nextLayout))
+            {
+                (currentLayout, nextLayout) = (nextLayout, currentLayout);
+            }
+
+            return currentLayout.SelectMany(row => row).Count(seat => seat == '#');
+        }
+
+        private bool SimulateRoundPart2(char[][] currentLayout, char[][] nextLayout)
+        {
+            var seatChanged = false;
+            for (var row = 0; row < currentLayout.Length; row++)
+            {
+                for (var col = 0; col < currentLayout[row].Length; col++)
+                {
+                    var currentSeat = currentLayout[row][col];
+                    if (currentSeat == '.')
+                    {
+                        continue;
+                    }
+
+                    var occupiedNeighbors = GetDirections()
+                        .Count(dir =>
+                        {
+                            var nextInDirection = AddVectors(dir, (col, row));
+                            while (IsInBounds(nextInDirection, currentLayout[row].Length, currentLayout.Length))
+                            {
+                                var seatState = currentLayout[nextInDirection.y][nextInDirection.x];
+                                if (seatState != '.')
+                                {
+                                    return seatState == '#';
+                                }
+
+                                nextInDirection = AddVectors(dir, nextInDirection);
+                            }
+
+                            return false;
+                        });
+
+                    var newState = currentSeat switch
+                    {
+                        'L' when occupiedNeighbors == 0 => '#',
+                        '#' when occupiedNeighbors >= 5 => 'L',
                         _ => currentSeat
                     };
 
@@ -86,9 +148,9 @@ namespace AdventOfCode2020.Solvers
             return (point1.x + point2.x, point1.y + point2.y);
         }
 
-        private long GetPart2Answer(List<string> rows)
+        private static bool IsInBounds((int x, int y) position, int width, int height)
         {
-            return -1;
+            return position.x >= 0 && position.x < width && position.y >= 0 && position.y < height;
         }
     }
 }
