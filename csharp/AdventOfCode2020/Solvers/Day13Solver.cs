52b39803b1dcd17ec8b3cf6e205b4417ddb78a0c
Eric Frees <efrees@atlasrfid.com>
2021-01-03 16:24:49 -0600

    Solve day 13, part 1 in C#

---
+++ csharp/AdventOfCode2020/Solvers/Day13Solver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2020.Solvers
+{
+    internal class Day13Solver : ISolver
+    {
+        private const string Name = "Day 13";
+        private const string InputFile = "day13input.txt";
+
+        public void Solve()
+        {
+            Console.WriteLine(Name);
+            var input = Input.GetLinesFromFile(InputFile).ToList();
+            var earliestTime = long.Parse(input[0]);
+            var busSchedule = input[1].Split(",");
+
+            Console.WriteLine($"Output (part 1): {GetPart1Answer(earliestTime, busSchedule)}");
+            Console.WriteLine($"Output (part 2): {GetPart2Answer(earliestTime, busSchedule)}");
+        }
+
+        private long GetPart1Answer(long earliestTime, string[] busSchedule)
+        {
+            var waitTimes = busSchedule
+                .Where(bus => bus != "x")
+                .Select(long.Parse)
+                .Select(period => (busId: period, waitTime: period - earliestTime % period));
+            var (busId, waitTime) = waitTimes.OrderBy(result => result.waitTime).First();
+            return busId * waitTime;
+        }
+
+        private long GetPart2Answer(long earliestTime, string[] busSchedule)
+        {
+            return -1;
+        }
+    }
+}
