a5f8963b11d3396a054a6516c4272be8d095c909
Eric Frees <efrees@atlasrfid.com>
2021-01-02 19:23:07 -0600

    Solve day 7, part 2 in C#

--- csharp/AdventOfCode2020/Solvers/Day07Solver.cs
+++ csharp/AdventOfCode2020/Solvers/Day07Solver.cs
@@ -16,7 +16,7 @@ namespace AdventOfCode2020.Solvers
             var nodes = Input.GetLinesFromFile(InputFile).Select(ParseBag).ToList();
 
             Console.WriteLine($"Output (part 1): {GetPart1Answer(nodes)}");
-            Console.WriteLine($"Output (part 2): {GetPart2Answer()}");
+            Console.WriteLine($"Output (part 2): {GetPart2Answer(nodes)}");
         }
 
         private BagNode ParseBag(string line)
@@ -27,13 +27,20 @@ namespace AdventOfCode2020.Solvers
             return new BagNode
             {
                 Color = name,
-                Contents = contents.Select(match => match.Groups[2].Value).ToList()
+                Contents = contents.Select(match => new CountedBags
+                {
+                    Count = int.Parse(match.Groups[1].Value),
+                    Color = match.Groups[2].Value
+                }).ToList()
             };
         }
 
         private int GetPart1Answer(List<BagNode> bagNodes)
         {
-            var containingColors = bagNodes.SelectMany(node => node.Contents.Select(child => (child, parent: node.Color)))
+            var containingColors = bagNodes
+                .SelectMany(node =>
+                    node.Contents.Select(child => (child: child.Color, parent: node.Color))
+                )
                 .ToLookup(x => x.child, x => x.parent);
             var visited = new HashSet<string>();
 
@@ -51,15 +58,37 @@ namespace AdventOfCode2020.Solvers
             return DepthFirstCount("shiny gold") - 1;
         }
 
-        private int GetPart2Answer()
+        private int GetPart2Answer(List<BagNode> bagNodes)
         {
-            return -1;
+            var contents = bagNodes
+                .ToDictionary(x => x.Color, x => x.Contents);
+            var memos = new Dictionary<string, int>();
+
+            int DepthFirstCount(string color)
+            {
+                if (memos.ContainsKey(color))
+                {
+                    return memos[color];
+                }
+
+                var totalCount = 1 + contents[color].Select(c => c.Count * DepthFirstCount(c.Color)).Sum();
+                memos.Add(color, totalCount);
+                return totalCount;
+            }
+
+            return DepthFirstCount("shiny gold") - 1;
         }
 
         private class BagNode
         {
             public string Color { get; set; }
-            public List<string> Contents { get; set; }
+            public List<CountedBags> Contents { get; set; }
+        }
+
+        private class CountedBags
+        {
+            public int Count { get; set; }
+            public string Color { get; set; }
         }
     }
 }
