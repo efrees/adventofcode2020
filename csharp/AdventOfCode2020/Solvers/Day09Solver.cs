fd0bfe15911f224739da3b8ae922d6c74c5c2ef0
Eric Frees <efrees@atlasrfid.com>
2021-01-02 20:42:04 -0600

    Solve day 9, part 2 in C#

--- csharp/AdventOfCode2020/Solvers/Day09Solver.cs
+++ csharp/AdventOfCode2020/Solvers/Day09Solver.cs
@@ -14,8 +14,9 @@ namespace AdventOfCode2020.Solvers
             Console.WriteLine(Name);
             var sequence = Input.GetLinesFromFile(InputFile).Select(long.Parse).ToList();
 
-            Console.WriteLine($"Output (part 1): {GetPart1Answer(sequence)}");
-            Console.WriteLine($"Output (part 2): {GetPart2Answer()}");
+            var part1Answer = GetPart1Answer(sequence);
+            Console.WriteLine($"Output (part 1): {part1Answer}");
+            Console.WriteLine($"Output (part 2): {GetPart2Answer(sequence, part1Answer)}");
         }
 
         private long GetPart1Answer(List<long> sequence)
@@ -45,8 +46,27 @@ namespace AdventOfCode2020.Solvers
             return -1;
         }
 
-        private int GetPart2Answer()
+        private long GetPart2Answer(List<long> sequence, long target)
         {
+            var buffer = new LinkedList<long>();
+            var sum = 0L;
+            foreach (var number in sequence)
+            {
+                buffer.AddLast(number);
+                sum += number;
+
+                while (sum > target && buffer.Any())
+                {
+                    sum -= buffer.First!.Value;
+                    buffer.RemoveFirst();
+                }
+
+                if (sum == target)
+                {
+                    return buffer.Max() + buffer.Min();
+                }
+            }
+
             return -1;
         }
     }
