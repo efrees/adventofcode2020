d4e6bff530645d525d71edea37ba6fc4a1b65e1a
Eric Frees <efrees@atlasrfid.com>
2021-01-05 20:46:56 -0600

    Solve day 17, part 2 in C#

--- csharp/AdventOfCode2020/Solvers/Day17Solver.cs
+++ csharp/AdventOfCode2020/Solvers/Day17Solver.cs
@@ -20,14 +20,14 @@ namespace AdventOfCode2020.Solvers
 
         private long GetPart1Answer(List<string> sequence)
         {
-            var grid = InitializeGrid(sequence);
+            var grid = InitializeGrid3d(sequence);
             var nextGrid = new HashSet<(int, int, int)>();
 
             var cycleCount = 6;
             var neighborCounts = new Dictionary<(int, int, int), int>();
             foreach (var cycle in Enumerable.Range(0, cycleCount))
             {
-                foreach (var neighbor in grid.SelectMany(GetNeighbors))
+                foreach (var neighbor in grid.SelectMany(GetNeighbors3d))
                 {
                     neighborCounts[neighbor] = neighborCounts.GetValueOrDefault(neighbor, 0) + 1;
                 }
@@ -40,14 +40,6 @@ namespace AdventOfCode2020.Solvers
                     }
                 }
 
-                //foreach (var activePoint in grid)
-                //{
-                //    var neighborCount = neighborCounts.GetValueOrDefault(activePoint, -1);
-                //    if (neighborCount == 2)
-                //    {
-                //        nextGrid.Add(activePoint);
-                //    }
-                //}
                 (grid, nextGrid) = (nextGrid, grid);
 
                 neighborCounts.Clear();
@@ -59,10 +51,36 @@ namespace AdventOfCode2020.Solvers
 
         private long GetPart2Answer(List<string> sequence)
         {
-            return -1;
+            var grid = InitializeGrid4d(sequence);
+            var nextGrid = new HashSet<(int, int, int, int)>();
+
+            var cycleCount = 6;
+            var neighborCounts = new Dictionary<(int, int, int, int), int>();
+            foreach (var cycle in Enumerable.Range(0, cycleCount))
+            {
+                foreach (var neighbor in grid.SelectMany(GetNeighbors4d))
+                {
+                    neighborCounts[neighbor] = neighborCounts.GetValueOrDefault(neighbor, 0) + 1;
+                }
+
+                foreach (var (point, neighborCount) in neighborCounts)
+                {
+                    if (neighborCount == 3 || (neighborCount == 2 && grid.Contains(point)))
+                    {
+                        nextGrid.Add(point);
+                    }
+                }
+
+                (grid, nextGrid) = (nextGrid, grid);
+
+                neighborCounts.Clear();
+                nextGrid.Clear();
+            }
+
+            return grid.Count;
         }
 
-        private static HashSet<(int, int, int)> InitializeGrid(IReadOnlyList<string> sequence)
+        private static HashSet<(int, int, int)> InitializeGrid3d(IReadOnlyList<string> sequence)
         {
             var grid = new HashSet<(int, int, int)>();
             for (var i = 0; i < sequence.Count; i++)
@@ -79,7 +97,24 @@ namespace AdventOfCode2020.Solvers
             return grid;
         }
 
-        private static IEnumerable<(int, int, int)> GetNeighbors((int x, int y, int z) point)
+        private static HashSet<(int, int, int, int)> InitializeGrid4d(IReadOnlyList<string> sequence)
+        {
+            var grid = new HashSet<(int, int, int, int)>();
+            for (var i = 0; i < sequence.Count; i++)
+            {
+                for (var j = 0; j < sequence[i].Length; j++)
+                {
+                    if (sequence[i][j] == '#')
+                    {
+                        grid.Add((i, j, 0, 0));
+                    }
+                }
+            }
+
+            return grid;
+        }
+
+        private static IEnumerable<(int, int, int)> GetNeighbors3d((int x, int y, int z) point)
         {
             for (var i = -1; i <= 1; i++)
             {
@@ -87,8 +122,34 @@ namespace AdventOfCode2020.Solvers
                 {
                     for (var k = -1; k <= 1; k++)
                     {
-                        if (i == 0 && j == 0 && k == 0) continue;
+                        if (i == 0 && j == 0 && k == 0)
+                        {
+                            continue;
+                        }
+
                         yield return (point.x + i, point.y + j, point.z + k);
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<(int, int, int, int)> GetNeighbors4d((int x, int y, int z, int w) point)
+        {
+            for (var i = -1; i <= 1; i++)
+            {
+                for (var j = -1; j <= 1; j++)
+                {
+                    for (var k = -1; k <= 1; k++)
+                    {
+                        for (var m = -1; m <= 1; m++)
+                        {
+                            if (i == 0 && j == 0 && k == 0 && m == 0)
+                            {
+                                continue;
+                            }
+
+                            yield return (point.x + i, point.y + j, point.z + k, point.w + m);
+                        }
                     }
                 }
             }
